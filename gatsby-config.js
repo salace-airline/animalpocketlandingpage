5bc2282b95512296a5e4f588643a5650665555cf
Sarah Watremet <sarah.watremet@gmail.com>
2023-06-23 11:49:34 +0200

    Remove Layout component + add Netlify plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
     title: `AnimalPocketWebsite`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-netlify", {
     resolve: 'gatsby-source-filesystem',
     options: {
       "name": "images",
